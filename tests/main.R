78c1aa85748867146f08086ac7d1bce626b91576
Valeria Callioni <leoniocallioni@MacBook-Pro-di-Leonio.local>
2018-12-10 12:29:45 +0100

    R package test

--- tests/main.R
+++ tests/main.R
@@ -7,6 +7,7 @@ library(RcppEigen)
 dyn.load("/vagrant/PACSProject/StreamNetgstat/src/interface.so")
 source("/vagrant/PACSProject/get_SSN_model.R")
 source("/vagrant/PACSProject/get_SSN_model_kriging.R")
+source("/vagrant/PACSProject/do_SSN_kriging.R")
 load("/vagrant/PACSProject/Data/middlefork_predictObs.RData")
 
 # file.copy(system.file(file.path("lsndata", "MiddleFork04.ssn"), package = "SSN"), 
@@ -20,14 +21,25 @@ load("/vagrant/PACSProject/Data/middlefork_predictObs.RData")
 # Missouri arguments: 17, c("STREAM_AUG", "ELEV", "SLOPE"), "afvArea"
 
 
-result = get_SSN_model_kriging(c("Summer_mn", "ELEV_DEM"), "afvArea",
-                            c("Exponential.tailup", "Exponential.taildown"), 
-                            net_num, bin_tables, network_data, obs_points, pred_points, obs_data, pred_data)
+result = get_SSN_model(c("Summer_mn", "ELEV_DEM"), "afvArea",
+                            c("Exponential.tailup", "Exponential.taildown", "Exponential.Euclid"),
+                            net_num, bin_tables, network_data, obs_points, obs_data)
+
+# result = do_SSN_kriging(net_num, bin_tables, network_data,
+#                         obs_points, pred_points, obs_data, pred_data, 
+#                         c("Summer_mn", "ELEV_DEM"), "afvArea",
+#                         c("Exponential.tailup", "Exponential.taildown"), TRUE,
+#                         theta, covMatrix)
+
+# result = get_SSN_model_kriging(c("Summer_mn", "ELEV_DEM"), "afvArea",
+#                             c("Exponential.tailup", "Exponential.taildown"),
+#                             net_num, bin_tables, network_data, obs_points, pred_points, obs_data, pred_data)
+
 
 print("Theta:")
 print(result$optTheta)
 print("Beta:")
 print(result$betaValues)
 setwd("/vagrant/PACSProject/")
-# write(t(result$covMatrix), file = "covMat.txt", ncolumns = 1143)
-write(t(result$predictions), file = "predData.txt", ncolumns = 2)
+# write(t(result$covMatrix), file = "covMat.txt", ncolumns = 45)
+# write(t(result$predictions), file = "predData.txt", ncolumns = 2)
