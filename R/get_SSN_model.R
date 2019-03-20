ba2f358f0998c3bf8830bd5bc2ab342e3ed151cd
Valeria Callioni <leoniocallioni@MacBook-Pro-di-Leonio.local>
2019-03-20 16:50:04 +0100

    Corrections for R documentation

--- R/get_SSN_model.R
+++ R/get_SSN_model.R
@@ -4,7 +4,9 @@
 #' @param varNames a vector of strings, the names of the variables used in the model.
 #' @param weightVar a string indicating the name of the variable to compute the spatial weights.
 #' @param CorModels a vector of strings, the names of the covariance models. The individual models should be of different "types" tail-up, tail-down, Euclidean. The tailup models include: "Exponential.tailup", "LinearSill.tailup", "Spherical.tailup", 
+#'
 #' "Mariah.tailup" "Epanech.tailup"; tail-down models include: "Exponential.taildown", "LinearSill.taildown", "Spherical.taildown", "Mariah.taildown"; Euclidean distance models include: "Spherical.Euclid", "Gaussian.Euclid", "Exponential.Euclid", 
+#'
 #' "Cauchy.Euclid". The first 4 tailup and taildown models are described in Ver Hoef and Peterson (2010), and the 4 Euclidean distance models are standard spatial covariance models.
 #' @param useNugget If \code{FALSE} the nugget effect is not included in the model. Default to \code{TRUE}.
 #' @param singleNet an interger, indicating the network ID that is to be analysed. Default to \code{NULL}, so that the analysis is carried on the entire dataset.
